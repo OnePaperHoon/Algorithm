cb4ec03ae6dff7ac4dad51a7cb721b263160cc93
OnePaperHoon <84340857+OnePaperHoon@users.noreply.github.com>
2025-04-19 14:56:17 +0900

    [level 0] Title: 배열 조각하기, Time: 1.28 ms, Memory: 6.61 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<int> query) {
+    vector<int> answer;
+    
+    for (int i = 0; i < query.size(); i++)
+    {
+        if (i % 2 == 0)
+            arr = vector<int>(arr.begin(), arr.begin() + query[i] + 1);
+        else
+            arr = vector<int>(arr.begin() + query[i], arr.end());
+    }
+    answer = arr;
+    return answer;
+}
