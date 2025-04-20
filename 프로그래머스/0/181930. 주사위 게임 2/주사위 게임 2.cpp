7268c5a4601cc3a18b5196d68e2b4215e574f58d
OnePaperHoon <84340857+OnePaperHoon@users.noreply.github.com>
2025-04-21 00:56:32 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -1,17 +1,19 @@
 #include <string>
 #include <vector>
+#include <set>
+#include <math.h>
 
 using namespace std;
 
 int solution(int a, int b, int c) {
     int answer = 0;
-    if (a != b && a != c && b != c)
+    set<int> temp{a, b, c};
+    
+    if (temp.size() == 3)
         return a + b + c;
-    if (a != b && b == c 
-     || a != b && a == c
-     || a == b && b != c)
-        return (a + b + c) * (a * a + b * b + c * c);
-    if (a == b && b == c)
-        return (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+    if (temp.size() == 2)
+        return (a + b + c) * (pow(a, 2) + pow(b, 2) + pow(c, 2));
+    if (temp.size() == 1)
+        return (a + b + c) * (pow(a, 2) + pow(b, 2) + pow(c, 2)) * (pow(a, 3) + pow(b, 3) + pow(c, 3));
     return answer;
 }
